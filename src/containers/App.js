3dac5ffe26a9600dba12b270d06f5cff2597754f
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2016-09-26 18:35:03 +0300

    init folder structure and package.json

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-bootstrap';
+import logo from '../logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -10,9 +11,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid className="App-intro">
+          <Row>
+            <Col lg={12} md={12} sm={12} xs={12}>
+              test
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
